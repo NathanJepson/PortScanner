b12bcb93ed087e5fc2a9267d1920e0b86ed0beb7
KaliVM <nhjepson@gmail.com>
2020-10-16 20:18:50 -0400

    Added port scanning by specifying a range, and made the output nicer.

--- PortScan.py
+++ PortScan.py
@@ -17,9 +17,17 @@ def oneTCPScan(host,port):
     else:
         return True
 
+def printResult(resultPorts):
+    print ('========Scan Results========\n')
+    print ('Open ports:')
+    for x in resultPorts:
+        print (x)
+    print('=============================')
+
+print('Extracting input.')
 theParser = argparse.ArgumentParser()
 theParser.add_argument("host",help="IP address of target machine.")
-theParser.add_argument("--ports",help="A list of ports separated by commas, i.e., 9,53,80")
+theParser.add_argument("--ports",help="A list of ports separated by commas, or a range, i.e., 9,53,80 OR 9-99")
 theParser.add_argument("--protocol",help="TCP, UDP, or ICMP (TCP being the default)")
 args = theParser.parse_args()
 
@@ -42,26 +50,51 @@ ports_result = []
 #Port list not provided
 if (not ports_given):
     print('No ports given, ports 1-65535 will be probed.')
+    print('Scanning...')
     for port in range(1024): #Fix me, use 65535
         if (protocol == 'TCP'):
             if (oneTCPScan(host,port) == True):
                 ports_result.append(port)
-
+    printResult(ports_result)
+    exit()
 
 #Port list is provided
 else:
+    #Is your 'ports' argument a range?
+    x = re.search("^(\d)+-(\d)+$",args.ports)
+    if (x):
+        ports = port_range = args.ports.split("-")
+        upperBound = int(port_range[1])
+        lowerBound = int(port_range[0])
+        if (lowerBound  > upperBound):
+            print('The second element of your range should be greater than the first.')
+            exit()
+        if (upperBound > 65535 or lowerBound < 1):
+            print('Port range can only be between 1 and 65535')
+            exit()
+        print('Scanning...')
+        for port in range (lowerBound,upperBound+1):
+            if (protocol == 'TCP'):
+                if (oneTCPScan(host,port) == True):
+                        ports_result.append(port)
+    printResult(ports_result)
+    exit()
+
     #Is your port list a list of integers separated by commas?
     x = re.search("^(\d|,)+$",args.ports)
     if (not x):
         print("Your list of ports does not match the correct syntax (integers separated by commas, e.g., 8,99,100).")
+        print("Alternatively, you can use a range, e.g., 9-99")
         exit()
     #Does your port list contain double commas?
     x = re.search("^.*,,.*$",args.ports)
     if (x):
         print("Your list of ports is not in the correct format (integers separated by commas, e.g., 8,99.100).")
+        print("Alternatively, you can use a range, e.g., 9-99")
         exit()
     #Split port list into each individual port
     port_array = args.ports.split(",")
+    print('Scanning...')
     for port in port_array:
         if (port == ''):
             continue
@@ -73,4 +106,4 @@ else:
                 print('not handled')
             elif (protocol == 'ICMP'):
                 print('not handled too')
-print(ports_result)
+printResult(ports_result)
